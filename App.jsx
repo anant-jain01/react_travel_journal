30cd31d97a6385e1479028aac984e7284c21e004
anant-jain01 <jain.anant609@gmail.com>
2025-03-26 16:42:31 +0530

    modified:   components/Entry.jsx

--- App.jsx
+++ App.jsx
@@ -1,34 +1,23 @@
-import "data.js";
 import Entry from "./components/Entry.jsx";
 import Header from "./components/Header.jsx";
+import data from "/data.js";
 export default function App() {
+  const dataElements = data.map((data) => {
+    return (
+      <Entry
+        title={data.title}
+        country={data.country}
+        googleMapsLink={data.googleMapsLink}
+        dates={data.dates}
+        text={data.text}
+        image={data.img.src}
+      />
+    );
+  });
   return (
     <>
       <Header />
-      <Entry
-        title="Mount Fuji"
-        country="Japan"
-        googleMapsLink="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
-        dates="12 Jan 2021 - 24 Jan, 2021"
-        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        image="https://scrimba.com/links/travel-journal-japan-image-url"
-      />
-      <Entry
-        title="Geirangerfjord"
-        country="Norway"
-        googleMapsLink="https://maps.app.goo.gl/fhkJuBhmFDv47tiB7"
-        dates="01 Oct 2021 - 18 Nov 2021"
-        text="The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
-        image="https://scrimba.com/links/travel-journal-norway-image-url"
-      />
-      <Entry
-        title="Sydney Opera House"
-        country="Australia"
-        googleMapsLink="https://maps.app.goo.gl/Zr17SCrsJeCEKMd36"
-        dates="27 May 2021 - 8 Jun 2021"
-        text="The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour it is often regarded as one of the 20th century's most famous and distinctive buildings."
-        image="https://scrimba.com/links/travel-journal-australia-image-url"
-      />
+      <main>{dataElements}</main>
     </>
   );
 }
--- components/Entry.jsx
+++ components/Entry.jsx
@@ -14,7 +14,7 @@ export default function Entry(props) {
         <span className="country">{props.country}</span>
         <a href={props.googleMapsLink}>View on Google Maps</a>
         <h2 className="entry-title">{props.title}</h2>
-        <p className="trip-dates">{props.date}</p>
+        <p className="trip-dates">{props.dates}</p>
         <p className="entry-text">{props.text}</p>
       </div>
     </article>
