7d738b44b7ad058773ca5d967135a5030d0ddcca
anant-jain01 <jain.anant609@gmail.com>
2025-03-26 14:43:45 +0530

    modified:   components/Entry.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header.jsx";
+import Entry from "./components/Entry.jsx";
+export default function App(){
+  return(
+    <>
+    <Header/>
+    <Entry/>
+    </>
+  )
+}
